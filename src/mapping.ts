af5a8e37ece9c3a8fe1eb0eaa613773b914a19fa
gwendal lasson <gwendal.lasson@gmail.com>
2021-07-14 10:51:02 +0200

    v1

--- src/mapping.ts
+++ src/mapping.ts
@@ -17,95 +17,68 @@ import {
   Unpause,
   Unpaused
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Round } from "../generated/schema"
 
 export function handleBetBear(event: BetBear): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.sender = event.params.sender
-  entity.currentEpoch = event.params.currentEpoch
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.TOTAL_RATE(...)
-  // - contract.adminAddress(...)
-  // - contract.bufferBlocks(...)
-  // - contract.claimable(...)
-  // - contract.currentEpoch(...)
-  // - contract.genesisLockOnce(...)
-  // - contract.genesisStartOnce(...)
-  // - contract.getUserRounds(...)
-  // - contract.intervalBlocks(...)
-  // - contract.ledger(...)
-  // - contract.minBetAmount(...)
-  // - contract.operatorAddress(...)
-  // - contract.oracleLatestRoundId(...)
-  // - contract.oracleUpdateAllowance(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.refundable(...)
-  // - contract.rewardRate(...)
-  // - contract.rounds(...)
-  // - contract.treasuryAmount(...)
-  // - contract.treasuryRate(...)
-  // - contract.userRounds(...)
+  var round = Round.load(event.params.currentEpoch.toHexString());
+  round.bearAmount = round.bearAmount.plus(event.params.amount);
+  round.totalAmount = round.totalAmount.plus(event.params.amount);
+  round.save();
 }
 
-export function handleBetBull(event: BetBull): void {}
+export function handleBetBull(event: BetBull): void {
+  var round = Round.load(event.params.currentEpoch.toHexString());
+  round.bullAmount = round.bullAmount.plus(event.params.amount);
+  round.totalAmount = round.totalAmount.plus(event.params.amount);
+  round.save();
+}
 
-export function handleClaim(event: Claim): void {}
+export function handleClaim(event: Claim): void { }
 
-export function handleClaimTreasury(event: ClaimTreasury): void {}
+export function handleClaimTreasury(event: ClaimTreasury): void { }
 
-export function handleEndRound(event: EndRound): void {}
+export function handleStartRound(event: StartRound): void {
+  var round = new Round(event.params.epoch.toHexString());
+  round.epoch = event.params.epoch;
+  round.startBlock = event.params.blockNumber;
+  round.bearAmount = BigInt.fromI32(0);
+  round.bullAmount = BigInt.fromI32(0);
+  round.totalAmount = BigInt.fromI32(0);
+  round.save();
+}
 
-export function handleLockRound(event: LockRound): void {}
+export function handleEndRound(event: EndRound): void {
+  var round = Round.load(event.params.epoch.toHexString());
+  round.endBlock = event.params.blockNumber;
+  round.closePrice = event.params.price;
+  round.save();
+}
 
-export function handleMinBetAmountUpdated(event: MinBetAmountUpdated): void {}
+export function handleLockRound(event: LockRound): void {
+  var round = Round.load(event.params.epoch.toHexString());
+  round.lockBlock = event.params.blockNumber;
+  round.lockPrice = event.params.price;
+  round.save();
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleRewardsCalculated(event: RewardsCalculated): void {
+  var round = Round.load(event.params.epoch.toHexString());
+  round.rewardAmount = event.params.rewardAmount;
+  round.rewardBaseCalAmount = event.params.rewardBaseCalAmount;
+  round.treasuryAmount = event.params.treasuryAmount;
+  round.save();
+}
 
-export function handlePause(event: Pause): void {}
+export function handleMinBetAmountUpdated(event: MinBetAmountUpdated): void { }
 
-export function handlePaused(event: Paused): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void { }
 
-export function handleRatesUpdated(event: RatesUpdated): void {}
+export function handlePause(event: Pause): void { }
 
-export function handleRewardsCalculated(event: RewardsCalculated): void {}
+export function handlePaused(event: Paused): void { }
 
-export function handleStartRound(event: StartRound): void {}
+export function handleRatesUpdated(event: RatesUpdated): void { }
 
-export function handleUnpause(event: Unpause): void {}
+export function handleUnpause(event: Unpause): void { }
 
-export function handleUnpaused(event: Unpaused): void {}
+export function handleUnpaused(event: Unpaused): void { }
