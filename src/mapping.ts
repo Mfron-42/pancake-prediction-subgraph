d1a658d64a6c4e4c40c21eb077d409723ffd6527
gwendal lasson <gwendal.lasson@gmail.com>
2021-07-17 13:18:26 +0200

    v

--- src/mapping.ts
+++ src/mapping.ts
@@ -17,19 +17,35 @@ import {
   Unpause,
   Unpaused
 } from "../generated/Contract/Contract"
-import { Round } from "../generated/schema"
+import { Bet, Round } from "../generated/schema"
 
 export function handleBetBear(event: BetBear): void {
-  var round = Round.load(event.params.currentEpoch.toHexString());
+  var roundId = event.params.currentEpoch.toHexString();
+  var round = Round.load(roundId);
   round.bearAmount = round.bearAmount.plus(event.params.amount);
   round.totalAmount = round.totalAmount.plus(event.params.amount);
+  var betAddress = event.params.sender.toHexString();
+  var bet = new Bet(roundId + "-" + betAddress);
+  bet.address = betAddress;
+  bet.amount = event.params.amount;
+  bet.position = "bear";
+  bet.save();
+  round.bets.push(bet.id);
   round.save();
 }
 
 export function handleBetBull(event: BetBull): void {
-  var round = Round.load(event.params.currentEpoch.toHexString());
+  var roundId = event.params.currentEpoch.toHexString();
+  var round = Round.load(roundId);
   round.bullAmount = round.bullAmount.plus(event.params.amount);
   round.totalAmount = round.totalAmount.plus(event.params.amount);
+  var betAddress = event.params.sender.toHexString();
+  var bet = new Bet(roundId + "-" + betAddress);
+  bet.address = betAddress;
+  bet.amount = event.params.amount;
+  bet.position = "bull";
+  bet.save();
+  round.bets.push(bet.id);
   round.save();
 }
 
@@ -39,16 +55,19 @@ export function handleClaimTreasury(event: ClaimTreasury): void { }
 
 export function handleStartRound(event: StartRound): void {
   var round = new Round(event.params.epoch.toHexString());
+  round.startTimestamp = event.block.timestamp;
   round.epoch = event.params.epoch;
   round.startBlock = event.params.blockNumber;
   round.bearAmount = BigInt.fromI32(0);
   round.bullAmount = BigInt.fromI32(0);
   round.totalAmount = BigInt.fromI32(0);
+  round.bets = new Array();
   round.save();
 }
 
 export function handleEndRound(event: EndRound): void {
   var round = Round.load(event.params.epoch.toHexString());
+  round.endTimestamp = event.block.timestamp;
   round.endBlock = event.params.blockNumber;
   round.closePrice = event.params.price;
   round.save();
@@ -56,6 +75,7 @@ export function handleEndRound(event: EndRound): void {
 
 export function handleLockRound(event: LockRound): void {
   var round = Round.load(event.params.epoch.toHexString());
+  round.lockTimestamp = event.block.timestamp;
   round.lockBlock = event.params.blockNumber;
   round.lockPrice = event.params.price;
   round.save();
